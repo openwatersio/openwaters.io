1c954506603fef923fb4f36b23b90befc40124a8
Brandon Keepers <brandon@opensoul.org>
2026-01-27 13:13:42 -0500

    Intial website created by my buddy Claude

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://openwaters.io',
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
+  vite: {
+    ssr: {
+      noExternal: ['maplibre-gl'],
+    },
+  },
+});
